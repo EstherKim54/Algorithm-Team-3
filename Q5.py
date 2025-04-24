4ce2e58f6034813f741130a02a709bb44e77885b
juyoung kim <22100180@handong.ac.kr>
2025-04-24 15:01:48 +0900

    Q5 수정

--- Q5.py
+++ Q5.py
@@ -9,26 +9,26 @@ def generate_number():
     numbers.append(a)
 
 def sorting():
-  generate_number()       #1,000,000개 리스트 생성
+    start_time = time.time()
+    generate_number()       #1,000,000개 리스트 생성
+  
+    numbers.sort()
+    end_time = time.time()
 
-  start_time = time.time()
-  numbers.sort()
-  end_time = time.time()
+    elapsed = end_time - start_time
 
-  elapsed = end_time - start_time
+    print(numbers[:10])
+    print(f"실행시간 : {elapsed:.2f}초")
 
-  print(numbers[:10])
-  print(f"실행시간 : {elapsed:.2f}초")
-
-  if elapsed and elapsed <3:
-    print('PASSCORD = ','TRUE')
-  else:
-    print('PASSCORD = ','FALSE')
+    if elapsed and elapsed <3:
+        print('PASSCORD = ','TRUE')
+    else:
+        print('PASSCORD = ','FALSE')
 
 sorting()
 
 '''
-[0, 2, 2, 3, 4, 7, 9, 9, 9, 10]
-실행시간 : 0.15초
+[2, 2, 3, 3, 3, 3, 5, 6, 6, 6]
+실행시간 : 0.68초
 PASSCORD =  TRUE
 '''
