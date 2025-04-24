9ac1d2d4f606ef0a94f813d96d5781759b4374c0
cklee <cklee@example.com>
2025-04-24 15:00:21 +0900

    Q3 수정

--- Q3.py
+++ Q3.py
@@ -1,17 +1,17 @@
 from collections import Counter
 
 def frequency_sort(nums: list[int]) -> list[tuple[int,int]]:
-    # 1) Counter 클래스로 nums 안 숫자들의 등장 횟수를 센다.
-    #    예: nums = [5,3,5,2] → Counter({5:2, 3:1, 2:1})
+    # Counter 클래스로 nums 안 숫자들의 등장 횟수를 센다.
+    # 예: nums = [5,3,5,2] → Counter({5:2, 3:1, 2:1})
     freq = Counter(nums)
 
-    # 2) freq.items() = [(5,2), (3,1), (2,1)] 형태의 리스트를 가져온 다음,
-    #    sorted()로 '(-횟수, -숫자)' 기준 정렬.
-    #    여기서 -를 붙이면 내림차순이 된다.
-    #    → 횟수 큰 순, 횟수 같으면 숫자 큰 순
+    # freq.items() = [(5,2), (3,1), (2,1)] 형태의 리스트를 가져온 다음,
+    # sorted()로 '(-횟수, -숫자)' 기준 정렬.
+    # 여기서 -를 붙이면 내림차순이 된다.
+    # → 횟수 큰 순, 횟수 같으면 숫자 큰 순
     sorted_items = sorted(freq.items(), key=lambda x: (-x[1], -x[0]))
     
-    # 3) 정렬된 (숫자,횟수) 리스트 반환
+    # 정렬된 (숫자,횟수) 리스트 반환
     return sorted_items
 
 if __name__ == "__main__":
@@ -28,12 +28,17 @@ if __name__ == "__main__":
     8, 9, 0, 2, 1, 3, 5, 4, 6, 7]
 
 
-    # 5) 함수 호출
+    # 함수 호출
     result = frequency_sort(nums)
     print(result)
 
     
-    # 6) 결과 출력
+    # 결과 출력
     passcord = result[0][0] + result[0][1]
   
     print(f"PASSCORD =", result[0][0], "+", result[0][1], "=", passcord)
+
+    '''
+    [(5, 12), (4, 12), (2, 12), (3, 11), (0, 11), (1, 10), (9, 9), (6, 9), (8, 7), (7, 7)]
+    PASSCORD = 5 + 12 = 17
+    '''
