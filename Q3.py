68f6f47c0a3910758008b145f6ccb3754575d0c7
cklee <cklee@example.com>
2025-04-24 19:45:21 +0900

    Q3 Comment Update

--- Q3.py
+++ Q3.py
@@ -1,14 +1,11 @@
 from collections import Counter
 
 def frequency_sort(nums: list[int]) -> list[tuple[int,int]]:
-    # Counter 클래스로 nums 안 숫자들의 등장 횟수를 센다.
-    # 예: nums = [5,3,5,2] → Counter({5:2, 3:1, 2:1})
+    # Counter 클래스로 nums 안 숫자들의 등장 횟수를 센다. (예: nums = [5,3,5,2] → Counter({5:2, 3:1, 2:1}))
     freq = Counter(nums)
 
     # freq.items() = [(5,2), (3,1), (2,1)] 형태의 리스트를 가져온 다음,
-    # sorted()로 '(-횟수, -숫자)' 기준 정렬.
-    # 여기서 -를 붙이면 내림차순이 된다.
-    # → 횟수 큰 순, 횟수 같으면 숫자 큰 순
+    # sorted()로 '(횟수, 숫자)' 기준 정렬을 내림차 순으로 한다(-) → 횟수 큰 순, 횟수 같으면 숫자 큰 순
     sorted_items = sorted(freq.items(), key=lambda x: (-x[1], -x[0]))
     
     # 정렬된 (숫자,횟수) 리스트 반환
@@ -30,13 +27,16 @@ if __name__ == "__main__":
 
     # 함수 호출
     result = frequency_sort(nums)
+
+    # 정렬된 숫자, 횟수 리스트 결과 출력
     print(result)
 
-    # 결과 출력
+    # 정렬된 리스트의 첫 번째 항목에 대해 숫자와  빈도수의 합(PASSCORD) 출력
     passcord = result[0][0] + result[0][1]
-  
     print(f"PASSCORD =", result[0][0], "+", result[0][1], "=", passcord)
 
+
+    # 최종 출력 결과
     '''
     [(5, 12), (4, 12), (2, 12), (3, 11), (0, 11), (1, 10), (9, 9), (6, 9), (8, 7), (7, 7)]
     PASSCORD = 5 + 12 = 17
